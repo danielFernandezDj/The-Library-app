99bad954c9a6d1d1c702ee71aa34c8fa6fed54c2
Daniel Fernandez <mdanif9728@gmail.com>
2024-11-04 09:56:34 -0800

    first commit

--- src/components/ImgView.jsx
+++ src/components/ImgView.jsx
@@ -1,32 +1,10 @@
 import React from "react";
 import { Button, } from "@nextui-org/button";
+import DownloadBtn from "./DownloadBtn"
 
 export default function ImgView() {
 
-  const handleDownload = async () => {
-    const fileUrl = "https://static.fotor.com/app/features/img/bgremove-banner-v2-t.jpg"
-
-    try {
-      // Fetch the file from the URL
-      const response = await fetch(fileUrl);
-      const blob = await response.blob();
-
-      // Create a temporary link element
-      const link = document.createElement("a");
-      link.href = URL.createObjectURL(blob);
-      link.download = "downloaded-image.jpg"; // File name for the download
-
-      // Append link to the body, trigger download, and remove link
-      document.body.appendChild(link);
-      link.click();
-      document.body.removeChild(link);
-
-      // Release the object URL
-      URL.revokeObjectURL(link.href);
-    } catch (error) {
-      console.error("Download failed:", error);
-    }
-  };
+  
 
 
   return (

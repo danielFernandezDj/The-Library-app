a1ef729f0a493712b57608163fc77a9e6bc1c116
Daniel Fernandez <mdanif9728@gmail.com>
2024-11-12 20:48:34 -0800

    level up the design to be a more consistent

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,12 @@ import Header from './components/Header';
 import BackgroundImage from './components/BackgroundImage';
 import Footer from './components/Footer';
 
-// Next UI
-import { Image, Button } from "@nextui-org/react";
-
-// Icons
-import { CircleArrowLeft, CircleArrowRight, UserRoundPen } from 'lucide-react';
-
 // Main App
 function App() {
   return (
     <>
       <main className="flex justify-center items-center h-dvh bg-current">
-        <section className='panel-bg-img'>
+        <section className='principal-panel'>
           <Header />
           <BackgroundImage />
           <Footer />

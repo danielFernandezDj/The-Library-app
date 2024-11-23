93d8aae0850aaa9ba43d8b9c665b3be52c25404a
Daniel Fernandez <mdanif9728@gmail.com>
2024-11-22 22:06:53 -0800

    saving vefore delete the folder

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -7,31 +7,7 @@ export default function Body() {
     <>
       <div className="body-panel">
         <dvi>
-          <p className="text-4xl w-3/5">
-            Im here bajsfhoiajstvmlaksdjl bajsfhoiajstvmlaksdjl bajsfhoiajstvmlaksdjl bajsfhoiajstvmlaksdjl bajsfhoiajstvmlaksdjl
-            Im here bajsfhoiajstvmlaksdjl bajsfhoiajstvmlaksdjl bajsfhoiajstvmlaksdjl bajsfhoiajstvmlaksdjl bajsfhoiajstvmlaksdjl
-            Im here bajsfhoiajstvmlaksdjl bajsfhoiajstvmlaksdjl bajsfhoiajstvmlaksdjl bajsfhoiajstvmlaksdjl bajsfhoiajstvmlaksdjl
-            Im here bajsfhoiajstvmlaksdjl bajsfhoiajstvmlaksdjl bajsfhoiajstvmlaksdjl bajsfhoiajstvmlaksdjl bajsfhoiajstvmlaksdjl
-            Im here bajsfhoiajstvmlaksdjl bajsfhoiajstvmlaksdjl bajsfhoiajstvmlaksdjl bajsfhoiajstvmlaksdjl bajsfhoiajstvmlaksdjl
-            Im here bajsfhoiajstvmlaksdjl bajsfhoiajstvmlaksdjl bajsfhoiajstvmlaksdjl bajsfhoiajstvmlaksdjl bajsfhoiajstvmlaksdjl
-            Im here bajsfhoiajstvmlaksdjl bajsfhoiajstvmlaksdjl bajsfhoiajstvmlaksdjl bajsfhoiajstvmlaksdjl bajsfhoiajstvmlaksdjl
-            Im here bajsfhoiajstvmlaksdjl bajsfhoiajstvmlaksdjl bajsfhoiajstvmlaksdjl bajsfhoiajstvmlaksdjl bajsfhoiajstvmlaksdjl
-            Im here bajsfhoiajstvmlaksdjl bajsfhoiajstvmlaksdjl bajsfhoiajstvmlaksdjl bajsfhoiajstvmlaksdjl bajsfhoiajstvmlaksdjl
-            Im here bajsfhoiajstvmlaksdjl bajsfhoiajstvmlaksdjl bajsfhoiajstvmlaksdjl bajsfhoiajstvmlaksdjl bajsfhoiajstvmlaksdjl
-            Im here bajsfhoiajstvmlaksdjl bajsfhoiajstvmlaksdjl bajsfhoiajstvmlaksdjl bajsfhoiajstvmlaksdjl bajsfhoiajstvmlaksdjl
-            Im here bajsfhoiajstvmlaksdjl bajsfhoiajstvmlaksdjl bajsfhoiajstvmlaksdjl bajsfhoiajstvmlaksdjl bajsfhoiajstvmlaksdjl
-            Im here bajsfhoiajstvmlaksdjl bajsfhoiajstvmlaksdjl bajsfhoiajstvmlaksdjl bajsfhoiajstvmlaksdjl bajsfhoiajstvmlaksdjl
-            Im here bajsfhoiajstvmlaksdjl bajsfhoiajstvmlaksdjl bajsfhoiajstvmlaksdjl bajsfhoiajstvmlaksdjl bajsfhoiajstvmlaksdjl
-            Im here bajsfhoiajstvmlaksdjl bajsfhoiajstvmlaksdjl bajsfhoiajstvmlaksdjl bajsfhoiajstvmlaksdjl bajsfhoiajstvmlaksdjl
-            Im here bajsfhoiajstvmlaksdjl bajsfhoiajstvmlaksdjl bajsfhoiajstvmlaksdjl bajsfhoiajstvmlaksdjl bajsfhoiajstvmlaksdjl
-            Im here bajsfhoiajstvmlaksdjl bajsfhoiajstvmlaksdjl bajsfhoiajstvmlaksdjl bajsfhoiajstvmlaksdjl bajsfhoiajstvmlaksdjl
-            Im here bajsfhoiajstvmlaksdjl bajsfhoiajstvmlaksdjl bajsfhoiajstvmlaksdjl bajsfhoiajstvmlaksdjl bajsfhoiajstvmlaksdjl
-            Im here bajsfhoiajstvmlaksdjl bajsfhoiajstvmlaksdjl bajsfhoiajstvmlaksdjl bajsfhoiajstvmlaksdjl bajsfhoiajstvmlaksdjl
-            Im here bajsfhoiajstvmlaksdjl bajsfhoiajstvmlaksdjl bajsfhoiajstvmlaksdjl bajsfhoiajstvmlaksdjl bajsfhoiajstvmlaksdjl
-            Im here bajsfhoiajstvmlaksdjl bajsfhoiajstvmlaksdjl bajsfhoiajstvmlaksdjl bajsfhoiajstvmlaksdjl bajsfhoiajstvmlaksdjl
-            Im here bajsfhoiajstvmlaksdjl bajsfhoiajstvmlaksdjl bajsfhoiajstvmlaksdjl bajsfhoiajstvmlaksdjl bajsfhoiajstvmlaksdjl
-            Im here bajsfhoiajstvmlaksdjl bajsfhoiajstvmlaksdjl bajsfhoiajstvmlaksdjl bajsfhoiajstvmlaksdjl bajsfhoiajstvmlaksdjl
-          </p>
+          
         </dvi>
       </div>
     </>
